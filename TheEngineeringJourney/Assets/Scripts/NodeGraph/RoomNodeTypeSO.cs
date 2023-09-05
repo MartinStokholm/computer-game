a60678aad910383c42df420d141148fb32910e78
BjarkeKarlsen <37935745+BjarkeKarlsen@users.noreply.github.com>
2023-09-05 11:42:51 +0200

    Add project

---
+++ TheEngineeringJourney/Assets/Scripts/NodeGraph/RoomNodeTypeSO.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomNodeTypeSO : MonoBehaviour
+{
+    public string roomNodeTypeName;
+
+    #region Header
+
+    [Header("Only flag the RoomNodeTypes that should be visible in the editor")]
+
+    #endregion
+    public bool displayInNodeGraphEditor = true;
+    
+    #region Header
+
+    [Header("One Type Should Be A corridor")]
+
+    #endregion
+    public bool isCorridor;
+    
+    #region Header
+
+    [Header("One Type Should Be A corridorNS")]
+
+    #endregion
+    public bool isCorridorNS;
+    
+    #region Header
+
+    [Header("One Type Should Be A corridorEW")]
+
+    #endregion
+    public bool isCorridorEW;
+    
+    #region Header
+
+    [Header("One Type Should Be A isEntrance")]
+
+    #endregion
+    public bool isEntrance;
+    
+    #region Header
+
+    [Header("One Type Should Be A Boss Room")]
+
+    #endregion
+    public bool isBossRoom;
+    
+    #region Header
+
+    [Header("One Type Should Be None (Unassigned")]
+
+    #endregion
+    public bool isNone;
+
+    #region Validation
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        HelperUtilities.ValidateCheckEmptyString(this, nameof(roomNodeTypeName), roomNodeTypeName);
+        
+    }
+#endif
+    #endregion
+}
---
+++ TheEngineeringJourney/Assets/Scripts/Utilities/HelperUtilities.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public static class HelperUtilities 
+{
+
+    /// <summary>
+    /// Empty string debug check
+    /// </summary>
+    public static bool ValidateCheckEmptyString(Object thisObject, string fieldName, string stringToCheck)
+    {
+        if (!string.IsNullOrEmpty(stringToCheck)) return false;
+        
+        Debug.Log($"{fieldName} is empty and must contain a value in object {thisObject.name}");
+        return true;
+    }
+    
+    /// <summary>
+    /// List empty or coa
+    /// </summary>
+    public static bool ValidateCheckEnumerableValues<T>(Object thisObject, string fieldName,
+        IEnumerable<T> enumerableObjectToCheck)
+    {
+        if (IsNull(enumerableObjectToCheck))
+        {
+            Debug.Log($"{fieldName} is empty and must contain a value in object {thisObject.name}");
+            return true;
+        }
+        
+        if (IsEmpty(enumerableObjectToCheck))
+        {
+            Debug.Log($"{fieldName} has no values in object {thisObject.name}");
+            return true;
+        }
+        
+        return false;
+    }
+    
+    private static bool IsNull<T>(this IEnumerable<T> enumerable) 
+    {
+        return enumerable == null;
+    }
+    
+    private static bool IsEmpty<T>(this IEnumerable<T> enumerable) 
+    {
+        return !enumerable.Any();
+    }
+}
