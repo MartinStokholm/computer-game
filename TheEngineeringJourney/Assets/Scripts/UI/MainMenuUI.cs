73b3f2f43e1fe3cba65f462ba23e08f6256e10a8
BjarkeKarlsen <bjarke.k22@gmail.com>
2023-11-26 14:32:41 +0100

    Start Menu - Bad but it works

---
+++ TheEngineeringJourney/Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField]private GameObject StartGameButton;
+    //[SerializeField]private GameObject LoadMenu;
+    [SerializeField]private GameObject ReturnToMainMenu;
+    [SerializeField]private GameObject QuitMenu;
+    [SerializeField]private GameObject TutorialMenu;
+    [SerializeField]private GameObject OptionsMenu;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        HideSubMenu();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainGameScene");
+    }
+
+    public void TutorialMenuButton()
+    {
+        TutorialMenu.SetActive(true);
+        ShowSubMenu();
+    }
+    
+    public void BackToPauseMenuButton()
+    {
+        HideSubMenu();
+        ViewDefaultMenu();
+    }
+    
+    public void OptionsMenuButton()
+    {
+        OptionsMenu.SetActive(true);
+        ShowSubMenu();
+    }
+    
+    public void QuitMenuButton()
+    {
+        QuitMenu.SetActive(true);
+        ShowSubMenu();
+    }
+    
+    public void QuitGameButton() => Application.Quit();
+    
+    private void  ViewDefaultMenu()
+    {
+        ReturnToMainMenu.SetActive(true);
+    }
+    
+    private void ShowSubMenu()
+    {
+        ReturnToMainMenu.SetActive(false);
+    }
+    
+    private void HideSubMenu()
+    {
+        TutorialMenu.SetActive(false);
+        OptionsMenu.SetActive(false);
+        QuitMenu.SetActive(false);
+    }
+}
